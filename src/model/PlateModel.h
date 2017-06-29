83f23112ed8b931cbe147602e0172845c7ed9bee
BluesJiang <763400095@qq.com>
2017-06-29 16:54:51 +0800

    add test and model

---
+++ src/model/PlateModel.h
@@ -0,0 +1,21 @@
+//
+// Created by BluesJiang on 2017/6/29.
+//
+
+#ifndef PLATERECOGNITION_PLATEMODEL_H
+#define PLATERECOGNITION_PLATEMODEL_H
+
+#include <iostream>
+
+class PlateModel {
+
+public:
+    std::string owner;
+    std::string plate;
+    std::string url;
+
+    PlateModel(const std::string &plate, const std::string &url, const std::string &owner = 0);
+};
+
+
+#endif //PLATERECOGNITION_PLATEMODEL_H
